f71a4f9ef2e99f6f315cfa2cb9a81264b71c55ae
emran-shaikh <emranshaikh@hotmail.com>
2022-04-20 23:24:55 +0500

    Error resolved for Netlify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import Users from './Users';
 import Student from './Student';
-import React, { Component, useState } from 'react';
-import { render } from '@testing-library/react';
+import React, { useState } from 'react';
 
 function App() {
 
-  let [name, setName] = useState('Emran');
-
-  function Apple(){
-    alert('Hello');
-  }
+  const [name, setName] = useState('Emran');
 
       return (
         <div className="App">
           <Student name={"Emran"} email={"emran@test.com"} />
-          <button onClick={()=>{setName('Shaikh')}}>Click Me</button>
+          <button onClick={()=>{setName('Shaikh')}}>Click me</button>
+          <Users />
         </div>
+        
     );
 
 }
